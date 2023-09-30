20443a80177cfa198b9c1a84d4e1ec5a66337d8c
Айаал Васильев <vasilievayaal@mail.ru>
2023-09-30 11:10:17 +0700

    Main.c

--- test/main.c
+++ test/main.c
@@ -1,8 +1,18 @@
 #define CTEST_MAIN
-
+/*
 #include <ctest.h>
 
 int main(int argc, const char** argv)
 {
     return ctest_main(argc, argv);
+}
+*/
+
+#include <stdio.h>
+
+int main(int argc, const char *argv[])
+{
+    while (*++argv)
+        puts(*argv);
+    return 0;
 }
